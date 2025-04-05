38cbd6cc777a139d69cd6526408cc208c60994fd
hschemmel <hannahschemmel@gmail.com>
2025-04-04 20:28:45 -0400

    Combined the home and about page, started working on the search page.

--- pathsacrossamerica/home/urls.py
+++ pathsacrossamerica/home/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home.index'),
-    path('about', views.about, name='home.about'),
 ]
--- pathsacrossamerica/running_places/views.py
+++ pathsacrossamerica/running_places/views.py
@@ -12,14 +12,21 @@ running_places = [
     },
 ]
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        running_places = RunningPlace.objects.filter(name__icontains=search_term)
+    else:
+        running_places = RunningPlace.objects.all()
     template_data = {}
     template_data['title'] = 'Running Places'
     template_data['running_places'] = RunningPlace.objects.all()
     return render(request, 'running_places/index.html', {'template_data': template_data})
 
 def show(request):
+    runningPlace = RunningPlace.objects.get(id=id)
     template_data = {}
-    template_data['title'] = 'Add Place'
+    template_data['title'] = runningPlace.name
+    template_data['runningPlace'] = runningPlace
     return render(request, 'running_places/show.html',
                   {'template_data': template_data})
 

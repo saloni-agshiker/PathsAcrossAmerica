c9bcb320b61441ee51dea567ad7dff8f822d46f8
hschemmel <hannahschemmel@gmail.com>
2025-04-06 17:17:06 -0400

    Fixing merge error

--- pathsacrossamerica/running_places/admin.py
+++ pathsacrossamerica/running_places/admin.py
@@ -3,8 +3,5 @@ from .models import RunningPlace, Review
 admin.site.register(RunningPlace)
 
 # Register your models here.
-class MovieAdmin(admin.ModelAdmin):
-    ordering = ['name']
-    search_fields = ['name']
-admin.site.register(RunningPlace, MovieAdmin)
+
 admin.site.register(Review)
